73aef5a57fcca2629a3a8c3c224b7417bf1b6be0
Ignatius Damai <ignatiusdamai@gmail.com>
2012-09-24 01:06:08 +0800

    Added superclass of object and DateObject subclass

--- YourDay/Object.h
+++ YourDay/Object.h
@@ -1,10 +1,9 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
-
 class Object {
 protected:
-	enum ObjectType { ENTRY_DATE, ENTRY_TIME, ENTRY_DETAILS, ENTRY_FULL_INFO };
+	enum ObjectType (ENTRY_DATE,ENTRY_TIME,ENTRY_DETAILS,ENTRY_INFO);
 	int objectType;
 public:
 	Object();
