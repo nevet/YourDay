5955033a6662119d1d353b521b918d0631cc1031
user <user@user-PC.mshome.net>
2012-09-25 05:32:58 +0800

    skeleton setup

--- YourDay/Main.cpp
+++ YourDay/Main.cpp
@@ -7,9 +7,80 @@
 
 using namespace std;
 
-enum TerminateSignal { SUCCESS };
+#define EXIT_SUCCESS 0
+
+/*
+This main is simply a skeleton, or pseudo-code... :S
+*/
 
 int main()
 {
-	return TerminateSignal::SUCCESS;
+	UIHandler ui;
+	LangHandler lang;
+	StorageHandler storage;
+	StatusHandler status;
+
+	Signal signal;
+
+	Object * obj;
+	
+	while (1)
+	{
+		ui.mainScreenDisplay();
+		signal = ui.getStatus();
+
+		if (status.success(signal))
+		{
+			ui.clearStatus();
+			ui.getInput();
+			signal = ui.getStatus();
+
+			if (status.success(signal))
+			{
+				obj = ui.retrieveObject();
+				lang.seperate(obj);
+				signal = lang.getStatus();
+
+				if (status.success(signal))
+				{
+					obj = lang.retrieve();
+					
+					switch (obj->getCommand())
+					{
+						case ADD:
+							storage.add(obj);
+							if (fail) output error message(same mechanism as previous)
+							break;
+
+						case DELETE:
+							storage.del(obj);
+							if (fail) output error message(same mechanism as previous)
+							break;
+
+						case UPDATE:
+							storage.upd(obj);
+							if (fail) output error message(same mechanism as previous)
+							break;
+
+						default:
+							break;
+					}
+				}
+				else
+				{
+					ui.errorDisplay(signal);
+				}
+			}
+			else
+			{
+				ui.errorDisplay(signal);
+			}
+		}
+		else
+		{
+			ui.errorDisplay(signal);
+		}
+	}
+	
+	return EXIT_SUCCESS;
 }
