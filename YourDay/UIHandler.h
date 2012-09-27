63aa2896e60c991de0d92872930e24896e0a8661
Nhu Thao <Nhu Thao@AS4750G-20062011.nus.edu.sg>
2012-09-27 14:21:34 +0800

    add UI files, which was removed due to conflict

---
+++ YourDay/UIHandler.h
@@ -0,0 +1,53 @@
+#ifndef UIHANDLER_H
+#define UIHANDLER_H
+
+#include <string>
+#include "Signal.h"
+#include "UI.h"
+#include "IO.h"
+#include "plainTextObject.h"
+
+using namespace std;
+
+class UIHandler
+{
+private:
+
+	/*These are successful display message*/
+	static const string ADD_SUCCESSFUL_MESSAGE;
+	static const string UPDATE_SUCCESSFUL_MESSAGE;
+	static const string DELETE_SUCCESSFUL_MESSAGE;
+
+	/*These are error display message*/
+	static const string DISPLAY_ERROR_MESSAGE;
+	static const string COMMAND_ERROR_MESSAGE;
+	static const string OPTION_ERROR_MESSAGE;
+
+	IO io;
+	UI ui;
+	Signal status;
+	plainTextObject* textObject;
+
+	void setStatus(Signal statusSignal);
+	void clearStatus();
+
+	void interpreteSignal(Signal outSignal, plainTextObject* outMessage);
+public:
+
+	UIHandler();
+
+	/*These are methods to get and print messages*/
+	void getInput();
+	void displayMessage(Signal outSignal);
+
+	/*These are methods to display UI*/
+	void startingScreenDisplay();
+	void mainScreenDisplay();
+
+	plainTextObject* retreiveObject();
+	Signal getStatus();
+
+	~UIHandler()
+};
+
+#endif
