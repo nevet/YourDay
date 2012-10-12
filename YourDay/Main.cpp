9cbccfb3e0ecdfd081fb368708b655f0b1d26a13
user <user@user-PC>
2012-10-12 20:28:01 +0800

    main() under new architecture

--- YourDay/Main.cpp
+++ YourDay/Main.cpp
@@ -1,118 +1,32 @@
 #include <iostream>
 #include <string>
-
-#include "Handler.h"
-#include "Object.h"
-#include "DateObject.h"
-#include "StatusHandler.h"
 #include "UIHandler.h"
-#include "LangHandler.h"
-#include "StorageHandler.h"
+#include "Signal.h"
 
 using namespace std;
 
 #define EXIT_SUCCESS 0
 
-/*
-This main is simply a skeleton, or pseudo-code... :S
-*/
-
 int main()
 {
-	UIHandler ui;
-	LangHandler lang;
-	StorageHandler storage;
-	StatusHandler status;
+	 FunctionHandler function;
+	 UIHandler ui;
+	 bool quit;
 
-	Signal signal;
+	 //which api should be main screen display?
+	 //@Nhu Thao plz change this line if this is incorrect.
+	 ui.mainScreenDisplay();
 
-	Object * obj;
-	
-	while (1)
-	{
-		ui.mainScreenDisplay();
-		signal = ui.getStatus();
+	 while (!quit)
+	 {
+		 ui.getInput();
+		 string* userInput = ui.retreiveObject();
 
-		if (status.success(signal))
-		{
-			ui.clearStatus();
-			ui.getInput();
-			signal = ui.getStatus();
+		 function.excute(userInput, quit);
 
-			if (status.success(signal))
-			{
-				obj = ui.retrieveObject();
-				lang.seperate(obj);
-				signal = lang.getStatus();
+		 Signal signal = function.status();
+		 ui.displayMessage(signal);
+	 }
 
-				if (status.success(signal))
-				{
-					obj = lang.retrieve();
-					
-					switch (static_cast<DetailsObject*>(obj)->getCommand())
-					{
-						case ADD:
-							storage.addEntry(obj);
-							signal = storage.getStatus();
-
-							if (status.success(signal))
-							{
-								ui.successDisplay(signal);
-							}
-							else
-							{
-								ui.errorDisplay(signal);
-							}
-
-							break;
-
-						case DELETE:
-							storage.delEntry(obj);
-
-							if (status.success(signal))
-							{
-								ui.successDisplay(signal);
-							}
-							else
-							{
-								ui.errorDisplay(signal);
-							}
-
-							break;
-
-						case UPDATE:
-							storage.updEntry(obj);
-							
-							if (status.success(signal))
-							{
-								ui.successDisplay(signal);
-							}
-							else
-							{
-								ui.errorDisplay(signal);
-							}
-
-							break;
-
-						default:
-							break;
-					}
-				}
-				else
-				{
-					ui.errorDisplay(signal);
-				}
-			}
-			else
-			{
-				ui.errorDisplay(signal);
-			}
-		}
-		else
-		{
-			ui.errorDisplay(signal);
-		}
-	}
-	
-	return EXIT_SUCCESS;
+	 return EXIT_SUCCESS;
 }
