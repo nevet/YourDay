14073e548cc7cb631214dcb7be6fc0ffb5a29e73
Nhu Thao <Nhu Thao@AS4750G-20062011.comp.nus.edu.sg>
2012-10-14 21:28:09 +0800

    updated UI

--- YourDay/UI.cpp
+++ YourDay/UI.cpp
@@ -1,5 +1,15 @@
+/**
+Your Day
+UI.h
+
+* This class is to display user interface
+* Display starting screen to user at the beginning of the program
+* Display main screen to interact with user
+* author: Nhu Thao
+*/
 #include "UI.h"
 
+//set the status of UI displaying proceess
 void UI::setStatus(Signal statusSignal)
 {
 	status = statusSignal;
@@ -9,21 +19,25 @@ UI::UI()
 {
 }
 
+//get status of UI displaying process
 Signal UI::getStatus()
 {
 	return status;
 }
 
+//clear status to default CLEAR signal
 void UI::clearStatus()
 {
 	status = CLEAR;
 }
 
+//display the starting screen at the beginning of the program
 void UI::startingScreenDisplay()
 {
 	setStatus(SUCCESS);
 }
 
+//display the main screen to interact with user
 void UI::mainScreenDisplay()
 {
 	setStatus(SUCCESS);
--- YourDay/UIHandler.h
+++ YourDay/UIHandler.h
@@ -1,3 +1,15 @@
+/**
+Your Day
+UIHandler.h
+
+* This class is used to handle user interface displaying and input/output processes
+* Control UI class to display user interface
+* Store and pass the user input string passed from IO class
+* Control IO class to display feedback message
+
+* @author: Nhu Thao
+*/
+
 #ifndef UIHANDLER_H
 #define UIHANDLER_H
 
@@ -29,21 +41,83 @@ private:
 	Signal status;
 	string input;
 
+	/**
+	set the status signal of UIHandler process
+
+	@param the success/error signal to be set to
+	@return void
+	*/
 	void setStatus(Signal statusSignal);
+
+	/**
+	inteprete the feedback signal to a string message to show to user
+
+	@param feedback signal
+	@return output string message
+	*/
 	string interpreteSignal(Signal outSignal);
 public:
 
 	UIHandler();
 
 	/*These are methods to get and print messages*/
+
+	/**
+	get and store the input message from IO
+	
+	@param void
+	@return void
+	*/
 	void getInput();
+
+	/**
+	display feedback message to user through IO
+
+	@param feedback signals from the program after processing user command
+	@return void	
+	*/
 	void displayMessage(Signal outSignal);
 
 	/*These are methods to display UI*/
+
+	/**
+	display the starting screen to user through UI at the beginning of the program
+
+	@param void
+	@return void
+	*/
 	void startingScreenDisplay();
+
+	/**
+	display the main screen through UI to interact with user
+
+	@param void
+	@return void
+	*/
 	void mainScreenDisplay();
 
+	/**
+	retrieve the input string entered by user
+
+	@param void
+	@return user input string
+	*/
 	string retrieveInput();
+
+	/**
+	get the status of UIHandler process
+
+	@param void
+	@return status signal of UIHandler process
+	*/
+	Signal getStatus();
+
+	/**
+	clear the status signal of UIHandler process to default CLEAR status
+
+	@param void
+	@return void
+	*/
 	void clearStatus();
 
 	~UIHandler()
