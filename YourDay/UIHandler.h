36c50c94bf6d0dbe80c1e1739b0a5897a353f6cc
Nhu Thao <Nhu Thao@AS4750G-20062011.comp.nus.edu.sg>
2012-10-15 02:54:35 +0800

    try

--- YourDay/UIHandler.h
+++ YourDay/UIHandler.h
@@ -22,6 +22,21 @@ class UIHandler:Handler
 {
 private:
 
+	
+	void createSomethingToCommit();
+	
+	void createSomethingToCommit();
+	void createSomethingToCommit();
+	void createSomethingToCommit();
+	void createSomethingToCommit();
+	void createSomethingToCommit();
+	void createSomethingToCommit();
+	void createSomethingToCommit();
+	/*alfkjslgkjgljlskfjal;jflsajfkslksl
+	*/
+
+
+
 	//These are successful display message and will be used in intepreteSignal operation to get the feedback message string
 	static const string CLEAR_SIGNAL_MESSAGE;
 	static const string ADD_SUCCESSFUL_MESSAGE;
