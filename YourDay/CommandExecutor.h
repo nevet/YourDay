9f7a25537af058c3933b37614d5f065bc6dab051
Nhu Thao <Nhu Thao@AS4750G-20062011.comp.nus.edu.sg>
2012-10-15 00:17:50 +0800

    added comment to CommandExecutor.h

--- YourDay/CommandExecutor.h
+++ YourDay/CommandExecutor.h
@@ -12,24 +12,26 @@ using namespace std;
 class CommandExecutor
 {
 private:
+	/**
+	* Status signal of command executing process. Signal will be:
+	* CLEAR		Testing signal;
+	* SUCCESS	Success indicator, all process are successfully handled;
+	* ERROR		Error indicator, some errors occured when executing.
+	*/
 	Signal status;
 	
-	/**
-	set the status signal of command execusion process
-
-	@param status signal to be set to
-	@return void
-	*/
 	void setStatus(Signal statusSignal);
-
+	
 	/**
-	add an entry as a pure string to entry list string vector
-
-	@param the entry string to be added
-	@return void
+	* This private method is to be called in executeCommand operation
+	* It will add an entry as a pure string to the entry list vector
+	* It takes in 2 parameters passed by executeCommand operation:
+	* @param entry list
+					is the pointer to the entry list vector
+	* @param eventDetails
+					is the string contains the entry to be added
 	*/
 	void addEntry(vector<string> * entryList, string eventDetails);
-
 	void deleteEntry(vector <string>* entryList, string entry);
 	void searchEntry(vector <string>* entryList, string keyWord);
 	void updateEntry(vector <string>* entryList, string entry);
@@ -37,27 +39,31 @@ private:
 public:
 	CommandExecutor();
 
-	/**
-	get the status signal of command execusion process
-
-	@param void
-	@return status signal of command execusion process
-	*/
 	Signal getStatus();
 
 	/**
-	clear the status of command execusion proces to default CLEAR signal
-
-	@param void
-	@return void
+	* This operation will clear the status signal to testing CLEAR signal 
 	*/
 	void clearStatus();
 
 	/**
-	execute user command
-
-	@param the entry list string vector, type signal of command, command detail string 
-	@return void
+	* This operation is the main logic operation in CommandExecutor class
+	* and will be called in the execute() operator inside FunctionHandler class
+	* It executes user command, manipulating the entry list
+	* It takes in 3 parameters passed by functionHandler class
+	* @param entryList
+	*				is the string vector pointer points to the list of events
+	* @param type
+	*				is the type of command, including:
+	*			ADD_COMMAND		: add an entry to the event list
+	*			DELETE_COMMAND	: delete an entry from the event list
+	*			EDIT_COMMAND	: edit an entry in the event list
+	*			SEARCH_COMMAND	: search for an entry containing a keyword in the event list
+	* @param detail
+	*				is the string to pass the command detail
+	*			For add command: it is the entry string to add to the event list
+	*			For delete and update command: it is the string specify the keyword in the entry to be delete/update
+	*			For search command: it is the key word to search in the event list
 	*/
 	void executeCommand(vector <string> * entryList, Signal type, string detail);
 
