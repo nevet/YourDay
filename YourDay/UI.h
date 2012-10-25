27e32229c452076d442748dbcecf047e0d7a4a98
Ignatius Damai <Ignatius Damai@IgnatiusDamai.nus.edu.sg>
2012-10-26 06:42:36 +0800

    updated the UI formatting a bit

---
+++ YourDay/UI.h
@@ -0,0 +1,77 @@
+/**
+* UI class is a part of UI part and will be called in the UIHandler
+* UI class is used to display User Interface to the screen
+* UI class has 2 main APIs to do this functionality:
+* startingScreenDisplay():void to diisplay starting screen to user at the beginning of the program
+* mainScreenDisplay():void to display main screen to interact with user
+*/
+#ifndef UI_H
+#define UI_H
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <cstdlib>
+#include <iomanip>
+#include <vector>
+#include "Signal.h"
+
+#define windowsHeight 25
+#define windowsWidth 80
+#define bottomBoardHeight 3
+#define boxHeight 1
+#define boardHeight (windowsHeight - boxHeight - bottomBoardHeight)
+#define boardWidth windowsWidth
+#define boxWidth windowsWidth
+#define generalEntryListInitX 0
+#define generalEntryListInitY 2
+#define calendarEntryListInitX 0
+#define calendarEntryListInitY 12
+
+#define INDEX_COLOR FOREGROUND_INTENSITY | FOREGROUND_BLUE
+#define DESCRIPTION_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE
+#define LOCATION_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN
+#define TIME_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN
+#define DATE_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN
+#define PRIORITY_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED
+
+using namespace std;
+
+class UI
+{
+private:
+	Signal status;
+	HANDLE hConsole;
+	char displayBoard[boardHeight][boardWidth];
+	void setStatus(Signal statusSignal);
+	void drawBanner();
+	void writeWords(string words, int startH, int startW);
+	void displayEntryList( vector<string>* calendarEntryList, vector<string> *generalEntryList );
+	
+public:
+
+	UI();
+	void coloredDisplayFormattedString(int,string);
+	void setNormal();
+	void drawBox();
+	void didUKnowBox();
+	Signal getStatus();
+	void setScreenSize();
+	void clearStatus();
+	void gotoxy(int x,int y);
+	/**
+	* This operation is used to display starting screen to the user at the beginning of the program
+	* and will be called in UIHandler class
+	*/
+	void startingScreenDisplay();
+
+	/**
+	* This operation is used to display the main screen to interact with the user
+	* and will be called in UIHandler class
+	*/
+	void mainScreenDisplay(vector<string>* calendarEntryList, vector<string>* generalEntryList);
+
+	~UI();
+};
+
+#endif
---
+++ YourDay/UIHandler.h
@@ -0,0 +1,142 @@
+/**
+* UIHandler is the main handler in the UI component and will be called in main(). UIHandler knows the IO and UI classes
+* UIHandler controls the screen displaying and input/output process
+* For screen displaying tasks: UIHandler has 2 public APIs startingScreenDisplay():void and mainScreenDisplay():void
+* For input/output tasks: UIHandler has APIs getInput():void, retrieveInput():string and displayMessage():void
+* UIHandler also has APIs setStatus() and clearStatus() to get and clear status signal
+* After each process, UIHandler will set the success/error signal to inform the main()
+*/
+
+#ifndef UIHANDLER_H
+#define UIHANDLER_H
+
+#include <string>
+#include <vector>
+#include "Signal.h"
+#include "UI.h"
+#include "IO.h"
+
+using namespace std;
+
+class UIHandler
+{
+private:
+
+	//These are successful display message and will be used in intepreteSignal operation to get the feedback message string
+	static const string CLEAR_SIGNAL_MESSAGE;
+	static const string ADD_SUCCESSFUL_MESSAGE;
+	static const string UPDATE_SUCCESSFUL_MESSAGE;
+	static const string DELETE_SUCCESSFUL_MESSAGE;
+
+	//These are error feedback messages on user input and will be used in intepreteSignal operation to get the feedback message string
+	static const string COMMAND_ERROR_MESSAGE;
+	static const string LENGTH_EXCEED_ERROR_MESSAGE;
+	static const string LENGTH_ZERO_ERROR_MESSAGE;
+	static const string INDEX_ERROR_MESSAGE;
+	static const string INVALID_DATE_ERROR_MESSAGE;
+	static const string INVALID_TIME_ERROR_MESSAGE;
+	static const string UNDO_ERROR_MESSAGE;
+
+	//These are error messages on displaying UI and will be used in intepreteSignal operation to get the feedback message string
+	static const string DISPLAY_ERROR_MESSAGE;
+
+	//These are error messages on executing commands and will be used in intepreteSignal operation to get the feedback message string
+	static const string NULL_EXECUTOR_ERROR_MESSAGE;
+	static const string ADD_FAILED_MESSAGE;
+	static const string DELETE_FAILED_MESSAGE;
+	static const string SEARCH_FAILED_MESSAGE;
+	static const string UPDATE_FAILED_MESSAGE;
+
+	//These are instance of IO and UI class, which will be called in operations of getting input,
+	// displaying message and displaying screen
+	IO io;
+	UI ui;
+	
+	/**
+	* Status signal of command executing process. Signal will be:
+	* CLEAR		Testing signal;
+	* SUCCESS	Success indicator, all process are successfully handled;
+	* ERROR		Error indicator, some errors occured when executing.
+	*/
+	Signal UIstatus;
+
+	//This attribute is used to store the user input passed from IO class
+	string input;
+
+	void setUIStatus(Signal statusSignal);
+
+	/**
+	* This operator is used to inteprete the following feedback signal:
+	* 	DISPLAY_E, COMMAND_E, OPTION_E, LENGTH_X_E, LENGTH_Z_E, EMPTY_ENTRY_E, 
+	*	ADD_S, UPDATE_SDELETE_S
+	* to a string feedback message
+	* It will be called in displayMessage(string):void operation
+	* @param outSignal
+	*				is the feedback message passed by the displayMessage operation
+	* @return: the feedback message string
+	*/
+	string interpreteSignal(Signal outSignal);
+
+	/**
+	* This operation is used to display the starting screen to the user at the beginning of the program
+	* It will call the UI to display starting screen
+	*/
+	void startingScreenDisplay();
+public:
+
+	/**
+	* When constructing instance of UIHandler, starting screen should be
+	* displayed and saved entries should be displayed on the screen as well
+	*/
+	UIHandler(vector<string>* calendarEntryList, vector<string>* generalEntryList);
+
+	//These are methods to get and print messages
+
+	/**
+	* This operation is used to get input message from the user
+	* It calls IO to get user input through command line and store it in the private string named input
+	*/
+	void getInput();
+
+	/**
+	* this operation is used to display output message to the user
+	* It takes in the feedback signal passed by main(), call private intepreteSignal(Signal):string
+	* to inteprete to a string and calls IO to output to the user
+	* @param outSignal
+	*				is the feedback signal passed by the main() to indicate the success/error in processing command
+	*/
+	void displayMessage(Signal outSignal);
+
+	/**
+	* This operation is used to display output messages to the user
+	* It takes in the result string vector passed in by main() and calls IO to display
+	* @param result
+				  is the result string vector to be displayed
+				  It can be the result of search command
+	*/
+	void displayMessage(vector<string>* result);
+
+	/**
+	* This operation is used to display output messages to the user
+	* It takes in the result string passed in by main() and calls IO to display
+	* @param result
+				  is the result string to be displayed
+	*/
+	void displayMessage(string result);
+	
+	//These are methods to display UI
+
+	/**
+	* This operation is used to display the main screen through UI to interact with user
+	* It will call the UI to display the main screen
+	*/
+	void mainScreenDisplay(vector<string>* calendarEntryList, vector<string>* generalEntryList);
+
+	string retrieveInput();
+	Signal getStatus();
+	void clearStatus();
+
+	~UIHandler();
+};
+
+#endif
