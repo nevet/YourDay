1ac595cb3645efa2a8ade6ac7e9e8d36dd708bb1
Nhu Thao <Nhu Thao@AS4750G-20062011.nus.edu.sg>
2012-10-26 07:16:11 +0800

    moved mainScreenDisplay function to main()

---
+++ YourDay/AddExecutor.cpp
@@ -0,0 +1,39 @@
+#include "AddExecutor.h"
+
+AddExecutor::AddExecutor(vector<string>* calendarEntryList, vector<string>* generalEntryList, string details)
+{
+	_generalEntryList = generalEntryList;
+	_calendarEntryList = calendarEntryList;
+	_details = details;
+
+	//a local copy of entry list for undo using
+	_undoCalendarEntryList = *calendarEntryList;
+	_undoGeneralEntryList = *generalEntryList;
+}
+
+void AddExecutor::execute() throw (string)
+{
+	bool isCalendarEntry = false;
+
+	if (_details == "")
+	{
+		throw string ("Cannot add an empty entry\n");
+	}
+
+	if (extractDate(_details) == "")
+	{
+		_generalEntryList -> push_back(_details);
+	}
+	else
+	{
+		_calendarEntryList -> push_back(_details);
+	}
+
+	status = ADD_S;
+}
+
+void AddExecutor::undo()
+{
+	*_generalEntryList = _undoGeneralEntryList;
+	*_calendarEntryList = _undoCalendarEntryList;
+}
---
+++ YourDay/DeleteExecutor.cpp
@@ -0,0 +1,34 @@
+#include "DeleteExecutor.h"
+
+DeleteExecutor::DeleteExecutor(vector<string>* entryList, string details)
+{
+	_entryList = entryList;
+	_details = details;
+
+	_undoEntryList = *entryList;
+}
+
+void DeleteExecutor::execute()
+{
+	int index;
+	
+	vector<string>::iterator position;
+	
+	index=extractIndex(_details);
+	if(index>_entryList->size())
+	{
+		throw string ("index is larger than list size\n");
+	}
+	else 
+	{
+		position=_entryList->begin()+index-1;
+		_entryList->erase(position);
+		
+		status = DELETE_S;
+	}
+}
+
+void DeleteExecutor::undo()
+{
+	*_entryList = _undoEntryList;
+}
