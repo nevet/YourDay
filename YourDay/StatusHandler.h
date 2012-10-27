cacb92df142552cc925fd511c5ad3e0ae0c18dfe
Nhu Thao <Nhu Thao@AS4750G-20062011.nus.edu.sg>
2012-10-27 15:03:31 +0800

    moved UIHandler to RemovedFiles folder

--- YourDay/StatusHandler.h
+++ YourDay/StatusHandler.h
@@ -1,41 +1,13 @@
 #ifndef STATUSHANDLER_H
 #define STATUSHANDLER_H
 
-#include <string>
 #include "Signal.h"
 
 using namespace std;
 
 class StatusHandler
 {
-private:
-	//These are successful display message and will be used in intepreteSignal operation to get the feedback message string
-	static const string CLEAR_SIGNAL_MESSAGE;
-	static const string ADD_SUCCESSFUL_MESSAGE;
-	static const string UPDATE_SUCCESSFUL_MESSAGE;
-	static const string DELETE_SUCCESSFUL_MESSAGE;
-
-	//These are error feedback messages on user input and will be used in intepreteSignal operation to get the feedback message string
-	static const string COMMAND_ERROR_MESSAGE;
-	static const string LENGTH_EXCEED_ERROR_MESSAGE;
-	static const string LENGTH_ZERO_ERROR_MESSAGE;
-	static const string INDEX_ERROR_MESSAGE;
-	static const string INVALID_DATE_ERROR_MESSAGE;
-	static const string INVALID_TIME_ERROR_MESSAGE;
-	static const string UNDO_ERROR_MESSAGE;
-
-	//These are error messages on displaying UI and will be used in intepreteSignal operation to get the feedback message string
-	static const string DISPLAY_ERROR_MESSAGE;
-
-	//These are error messages on executing commands and will be used in intepreteSignal operation to get the feedback message string
-	static const string NULL_EXECUTOR_ERROR_MESSAGE;
-	static const string ADD_FAILED_MESSAGE;
-	static const string DELETE_FAILED_MESSAGE;
-	static const string SEARCH_FAILED_MESSAGE;
-	static const string UPDATE_FAILED_MESSAGE;
 public:
-
-	string interpreteSignal(Signal outSignal);
 	bool success(Signal signal);
 	bool error(Signal signal);	
 };
--- YourDay/UI.cpp
+++ YourDay/UI.cpp
@@ -11,6 +11,133 @@
 
 #include "UI.h"
 
+//Set output strings for successful feedback signals
+const string UI::CLEAR_SIGNAL_MESSAGE = "Command done\n";
+const string UI::ADD_SUCCESSFUL_MESSAGE = "Added successfully\n";
+const string UI::UPDATE_SUCCESSFUL_MESSAGE = "Updated successfully\n";
+const string UI::DELETE_SUCCESSFUL_MESSAGE = "Deleted successfully\n";
+
+//set output strings for error feedback signals on user input
+const string UI::COMMAND_ERROR_MESSAGE = "Wrong command\n";
+const string UI::LENGTH_EXCEED_ERROR_MESSAGE = "Command length exceeded the valid range\n";
+const string UI::LENGTH_ZERO_ERROR_MESSAGE = "Cannot enter command with empty detail\n";
+const string UI::INDEX_ERROR_MESSAGE = "Please enter an index\n";
+const string UI::INVALID_DATE_ERROR_MESSAGE = "Invalid date\n";
+const string UI::INVALID_TIME_ERROR_MESSAGE = "Invalid time\n";
+const string UI::UNDO_ERROR_MESSAGE = "Nothing to undo\n";
+
+//set output strings for error feedback signals on displaying UI
+const string UI::DISPLAY_ERROR_MESSAGE = "Display error\n";
+
+//set output strings for error feedback signals on executing commands
+const string UI::NULL_EXECUTOR_ERROR_MESSAGE = "Executor pointer is NULL\n";
+const string UI::ADD_FAILED_MESSAGE = "Add failed\n";
+const string UI::DELETE_FAILED_MESSAGE = "Delete failed\n";
+const string UI::SEARCH_FAILED_MESSAGE = "Cannot find the key word\n";
+const string UI::UPDATE_FAILED_MESSAGE = "Update failed\n";
+
+string UI::interpreteSignal(Signal outSignal)
+{
+	string outString;
+
+	switch (outSignal)
+	{
+	case CLEAR:
+		{
+			outString = CLEAR_SIGNAL_MESSAGE;
+			break;
+		}
+	case ADD_S:
+		{
+			outString = ADD_SUCCESSFUL_MESSAGE;
+			break;
+		}
+	case  DELETE_S:
+		{
+			outString = DELETE_SUCCESSFUL_MESSAGE;
+			break;
+		}
+	case  UPDATE_S:
+		{
+			outString = UPDATE_SUCCESSFUL_MESSAGE;
+			break;
+		}
+	case  COMMAND_E:
+		{
+			outString = COMMAND_ERROR_MESSAGE;
+			break;
+		}
+	case  LENGTH_X_E:
+		{
+			outString = LENGTH_EXCEED_ERROR_MESSAGE;
+			break;
+		}
+	case  LENGTH_Z_E:
+		{
+			outString = LENGTH_ZERO_ERROR_MESSAGE;
+			break;
+		}
+	case  INDEX_E:
+		{
+			outString = INDEX_ERROR_MESSAGE;
+			break;
+		}
+	case  DATE_E:
+		{
+			outString = INVALID_DATE_ERROR_MESSAGE;
+			break;
+		}
+	case  TIME_E:
+		{
+			outString = INVALID_TIME_ERROR_MESSAGE;
+			break;
+		}
+	case  DISPLAY_E:
+		{
+			outString = DISPLAY_ERROR_MESSAGE;
+			break;
+		}
+	case  UNDO_E:
+		{
+			outString = UNDO_ERROR_MESSAGE;
+			break;
+		}
+	case  EXENULL_E:
+		{
+			outString = NULL_EXECUTOR_ERROR_MESSAGE;
+			break;
+		}
+	case  ADD_F:
+		{
+			outString = ADD_FAILED_MESSAGE;
+			break;
+		}
+	case  DELETE_F:
+		{
+			outString = DELETE_FAILED_MESSAGE;
+			break;
+		}
+	case  SEARCH_F:
+		{
+			outString = SEARCH_FAILED_MESSAGE;
+			break;
+		}
+	case  UPDATE_F:
+		{
+			outString = UPDATE_FAILED_MESSAGE;
+			break;
+		}
+
+	default:
+		{
+			outString = "";
+			break;
+		}
+	}
+
+	return outString;
+}
+
 
 void UI::setScreenSize()
 {
@@ -36,11 +163,6 @@ void UI::drawBanner()
 	cout<<"                                                                                ";
 }
 
-void UI::setStatus(Signal statusSignal)
-{
-	status = statusSignal;
-}
-
 void UI::writeWords(string words, int startX, int startY)
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY |
@@ -56,16 +178,6 @@ UI::UI(vector<string>* calendarEntryList, vector<string>* generalEntryList)
 	mainScreenDisplay(calendarEntryList,generalEntryList);
 }
 
-Signal UI::getStatus()
-{
-	return status;
-}
-
-void UI::clearStatus()
-{
-	status = CLEAR;
-}
-
 void UI::startingScreenDisplay()
 {
 	system("CLS");
@@ -78,7 +190,6 @@ void UI::startingScreenDisplay()
 	SetConsoleTextAttribute(hConsole, 15);
 	gotoxy(30,18);
 	cout<<"Press Enter to continue";
-	setStatus(SUCCESS);
 	getchar();
 }
 
@@ -116,7 +227,6 @@ void UI::mainScreenDisplay(vector<string>* calendarEntryList,vector<string>* gen
 	displayEntryList( calendarEntryList, generalEntryList );
 
 	drawBox();
-	setStatus(SUCCESS);
 }
 
 void UI :: gotoxy(int x, int y) //goes to x,y console
