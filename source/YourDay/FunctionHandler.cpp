6c02ec9cb95af1c4f2b084dc005927f694f5fb65
user <user@user-PC>
2012-11-02 07:48:10 +0800

    log compile bug fixed

--- source/YourDay/FunctionHandler.cpp
+++ source/YourDay/FunctionHandler.cpp
@@ -42,8 +42,6 @@ void FunctionHandler::execute(string input, bool* quit, Signal focusingField,
 							  vector<string>* calendarEntryList,
 							  vector<string>* diduknowBoxList) throw (string)
 {
-	log.write("logic component entered.");
-	
 	assert(generalEntryList!=NULL);
 	assert(calendarEntryList!=NULL);
 	assert(diduknowBoxList!=NULL);
