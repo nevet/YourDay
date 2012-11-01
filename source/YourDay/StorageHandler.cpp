03e74347fc5c8da13e33aeb39b919d703f6219c3
Nhu Thao <Nhu Thao@AS4750G-20062011.nus.edu.sg>
2012-11-02 06:44:28 +0800

    update diduknow (not workable)

---
+++ source/YourDay/StorageHandler.cpp
@@ -0,0 +1,133 @@
+#include <cassert>
+
+#include "StorageHandler.h"
+
+
+using namespace std; 
+
+
+const string StorageHandler::ENTRY_STORE_FORMAT = "%s %s %s\n";
+
+string StorageHandler::DataBaseGeneralFile = "YourDayGEntry.txt";
+string StorageHandler::DataBaseCalendarFile = "YourDayCEntry.txt";
+
+StorageHandler::StorageHandler()
+{
+	if(!checkFileExistence("",DataBaseGeneralFile))
+		ofstream writeFile(DataBaseGeneralFile);
+
+	if(!checkFileExistence("",DataBaseCalendarFile))
+		ofstream writeFile(DataBaseCalendarFile);
+}
+
+StorageHandler::~StorageHandler()
+{}
+
+void StorageHandler::setStatus()
+{
+	status = CLEAR;
+}
+
+void StorageHandler::readData(vector<string> *ramForGeneralList, vector<string>  *ramForCalendarList)
+{	
+	assert(ramForGeneralList!=NULL);
+	assert(ramForCalendarList!=NULL);
+
+	ifstream infileG(DataBaseGeneralFile);
+	string textLine;
+	while(getline(infileG,textLine))
+	{
+		ramForGeneralList->push_back(textLine);
+	}
+	infileG.close();
+
+	ifstream infileC(DataBaseCalendarFile);
+	while(getline(infileC,textLine))
+	{
+		ramForCalendarList->push_back(textLine);
+	}
+	infileC.close();
+
+	return ;
+}
+
+void StorageHandler::writeData(vector<string> *ramForGeneralList, vector<string>  *ramForCalendarList)
+{
+	assert(ramForGeneralList!=NULL);
+	assert(ramForCalendarList!=NULL);
+
+	ofstream clearFileG(DataBaseGeneralFile);
+	ofstream outfileG(DataBaseGeneralFile,ofstream::app);
+	
+	for(int i=0;i<ramForGeneralList->size();i++)
+	{
+		outfileG<<(ramForGeneralList->at(i))<<endl;
+	}
+	outfileG.close();
+
+	ofstream clearFileC(DataBaseCalendarFile);
+	ofstream outfileC(DataBaseCalendarFile,ofstream::app);
+
+	for(int i=0;i<ramForCalendarList->size();i++)
+	{
+		outfileC<<(ramForCalendarList->at(i))<<endl;
+	}
+	outfileC.close();
+
+	return ;
+}
+
+bool StorageHandler::checkFileExistence(string filePath, string fileName)
+{
+	fstream testFile;
+	bool isExist;
+	associateFile(filePath, fileName, testFile, OPEN_TYPE::IN_TYPE);
+	isExist=testFile.is_open();
+	disassociateFile(testFile);
+	return isExist;
+}
+
+void StorageHandler::disassociateFile(fstream & file)
+{
+	if (file.is_open()) file.close();
+}
+
+void StorageHandler::associateFile(string filePath, string fileName,
+	fstream & file, OPEN_TYPE mode)
+{
+	string fileFullPath(filePath+fileName);
+	switch (mode)
+	{
+	case APP:
+		file.open(fileFullPath.c_str(), fstream::app);
+		break;
+	case IN_TYPE:
+		file.open(fileFullPath.c_str(), fstream::in);
+		break;
+	case OUT_TYPE:
+		file.open(fileFullPath.c_str(), fstream::out);
+		break;
+	default:
+		break;
+	}
+}
+
+
+void StorageHandler::deleteFile(string filePath, string fileName)
+{
+	string fullPath(filePath+fileName);
+	remove(fullPath.c_str());
+}
+
+void StorageHandler::renameFile(string filePath, string oriName, string newName)
+{
+	string oriFullPath(filePath+oriName);
+	string newFullPath(filePath+newName);
+	rename(oriFullPath.c_str(), newFullPath.c_str());
+}
+
+void StorageHandler::replaceFile(string oriPath, string oriName, string repName)
+{
+	deleteFile(oriPath, oriName);
+	renameFile(oriPath, oriName, repName);
+}
---
+++ source/YourDay/UI.h
@@ -0,0 +1,118 @@
+#ifndef UI_H
+#define UI_H
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <cstdlib>
+#include <iomanip>
+#include <vector>
+#include <conio.h>
+#include "Signal.h"
+
+#define windowsHeight 40
+#define windowsWidth 120
+#define generalTitleHeight 2
+#define generalBoxHeight 12
+#define calendarTitleHeight 3
+#define calendarBoxHeight (windowsHeight-generalTitleHeight-generalBoxHeight-calendarTitleHeight-commandBoxHeight-bottomBoxHeight) 
+#define commandBoxHeight 2
+#define bottomBoxHeight 7
+
+#define generalInitY (generalTitleHeight)
+#define generalInitX 0
+#define calendarInitY (generalInitY + generalBoxHeight + calendarTitleHeight)
+#define calendarInitX 0
+#define commandInitY (calendarInitY + calendarBoxHeight)
+#define commandInitX 0
+#define diduknowInitY (calendarInitY + calendarBoxHeight + commandBoxHeight)
+#define diduknowInitX 0
+
+//INDEX_COLOR, DESCRIPTION_COLOR, LOCATION_COLOR, TIME_COLOR, DATE_COLOR, PRIORITY_COLOR
+#define calendarIndexInitX 2
+#define calendarDescriptionInitX 6
+#define calendarLocationInitX (calendarTimeInitX - 20)
+#define calendarTimeInitX 85
+#define calendarDateInitX 100
+#define calendarPriorityInitX 115
+
+#define generalIndexInitX 2
+#define generalDescriptionInitX 6
+#define generalLocationInitX (generalTimeInitX- 10)
+#define generalTimeInitX 115
+#define generalDateInitX 115
+#define generalPriorityInitX 115
+
+#define maxCharLocationCalendar (calendarTimeInitX - calendarLocationInitX -1)
+#define maxCharDetailCalendar (calendarLocationInitX - calendarDescriptionInitX -1)
+#define maxCharLocationGeneral (generalTimeInitX - generalLocationInitX -1)
+#define maxCharDetailGeneral (generalLocationInitX - generalDescriptionInitX -1)
+
+#define maxCharDetail 12
+
+#define INDEX_COLOR FOREGROUND_INTENSITY | FOREGROUND_BLUE
+#define DESCRIPTION_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE
+#define LOCATION_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN
+#define TIME_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN
+#define DATE_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN
+#define PRIORITY_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED
+
+#define TAB 9
+#define ENTER 13
+#define BACKSPACE 8
+
+#define maxInputSize 192
+
+using namespace std;
+
+class UI
+{
+private:
+
+	HANDLE hConsole;
+	string input;
+	Signal focusedField;
+	Signal displayMode;
+	int generalInitRowIndex;
+	int calendarInitRowIndex;
+	int diduknowInitRowIndex;
+
+	void setScreenSize();
+	void setBackground();
+	void drawBanner();
+	void drawCommandBox();
+	void clearBox(int startH, int height);
+	void gotoxy(int x, int y);
+	void writeTitle(string words, int startH, int startW);
+
+	void changeDisplayMode();
+	void displayNewMode(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* diduknowBoxList);
+	void changeFocusedField(vector<string>* diduknowBoxList);
+	void scrollUp(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* diduknowBoxList);
+	void scrollDown(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* diduknowBoxList);
+	void traceInput(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* diduknowBoxList);
+
+	void displayCalendarString(int index, string row, int &rowPosition);
+	void displayGeneralString(int index, string row, int &rowPosition);
+	void displayDiduknowString(int index, string row, int &rowPosition, int sizeOfGeneral);
+	bool isGeneral(string row);
+	void coloredDisplayFormattedString(int,string, int);
+
+	void generalEntryListDisplay(vector<string>* generalEntryList);
+	void calendarEntryListDisplay(vector<string>* calendarEntryList);
+	void diduknowBoxListDisplay(vector<string>* diduknowBoxList, int sizeOfGeneral);
+	void startingScreenDisplay();
+	void mainScreenDisplay(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* diduknowBoxList);
+public:
+	UI();
+
+	void userInteract(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* diduknowBoxList);
+	string retrieveInput();
+	Signal retrieveFocusedField();
+	void displayMessage(string message);
+	void displayMessage(Signal status);
+
+	~UI();
+};
+
+#endif;
