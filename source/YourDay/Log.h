2ed357032e4e45e1022e0097ab4ad5a1ad9eac06
user <user@user-PC>
2012-11-02 06:35:38 +0800

    Log semi implemented; LangHandler bug fixed;

---
+++ source/YourDay/Log.h
@@ -0,0 +1,36 @@
+#ifndef LOG_H
+#define LOG_H
+
+# include <string>
+# include <fstream>
+
+# include "Signal.h"
+
+using namespace std;
+
+class Log
+{
+private:
+
+	enum OPEN_TYPE {APP, OUT_TYPE};
+	
+	fstream file;
+	void disassociateFile();
+	void associateFile(string fileName, OPEN_TYPE mode);
+	
+public:
+	
+	Log();
+
+	void writeCreated(string objName);
+	void writeRetrived(string varName);
+	void writeExecuted(string fxName);
+	void writeConditionEntered(string condition, bool boolValue);
+	
+	void writeData(string dataName, string data);
+	void writeData(string dataName, Signal data);
+	
+	void writeException(string exception);
+}
+
+#endif
