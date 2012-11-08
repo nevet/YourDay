ffc954df98fdfc4b25945c8566c30e3f1f95f10e
Nhu Thao <Nhu Thao@AS4750G-20062011.nus.edu.sg>
2012-11-09 04:34:52 +0800

    new display of general list

---
+++ source/YourDay/Log.cpp
@@ -0,0 +1,106 @@
+# include "Log.h"
+
+void Log::disassociateFile()
+{
+	if (file.is_open()) file.close();
+}
+
+void Log::associateFile(string fileName, OPEN_TYPE mode)
+{
+	switch (mode)
+	{
+		case APP_TYPE:
+			file.open(fileName.c_str(), fstream::app);
+			
+			break;
+
+		case OUT_TYPE:
+			file.open(fileName.c_str(), fstream::out);
+			
+			break;
+		default:
+			break;
+	}
+}
+
+void Log::writeTime()
+{
+	time_t seconds = time(NULL);
+	struct tm * timeinfo = localtime(&seconds);
+	
+	associateFile("log.txt", APP_TYPE);
+	
+	file << timeinfo->tm_year + 1900 << "/" << timeinfo->tm_mon + 1 << "/" << timeinfo->tm_mday << endl;
+}
+
+void Log::writeCreated(string objName)
+{
+	associateFile("log.txt", APP_TYPE);
+	
+	file << objName << " is created." << endl;
+	
+	disassociateFile();
+}
+
+void Log::writeRetrieved(string varName)
+{
+	associateFile("log.txt", APP_TYPE);
+	
+	file << varName << " is retrieved." << endl;
+	
+	disassociateFile();
+}
+
+void Log::writeExecuted(string fxName)
+{
+	associateFile("log.txt", APP_TYPE);
+	
+	file << fxName << "is executed." << endl;
+	
+	disassociateFile();
+}
+
+void Log::writeConditionEntered(string condition, bool boolValue)
+{
+	associateFile("log.txt", APP_TYPE);
+	
+	file << "condition: " << condition << " is evaluated as " << boolValue << endl;
+	
+	disassociateFile();
+}
+	
+void Log::writeData(string dataName, string data)
+{
+	associateFile("log.txt", APP_TYPE);
+	
+	file << dataName << " = " << data << endl;
+	
+	disassociateFile();
+}
+
+void Log::writeData(string dataName, Signal data)
+{
+	associateFile("log.txt", APP_TYPE);
+	
+	file << dataName << " = " << data << endl;
+	
+	disassociateFile();
+}
+
+void Log::writeData(string dataName, bool data)
+{
+	associateFile("log.txt", APP_TYPE);
+	
+	file << dataName << " = " << data << endl;
+	
+	disassociateFile();
+}
+	
+void Log::writeException(string exception)
+{
+	associateFile("log.txt", APP_TYPE);
+	
+	file << "exception threw: " << exception << endl;
+	
+	disassociateFile();
+}
---
+++ source/YourDay/UI.h
@@ -0,0 +1,165 @@
+#ifndef UI_H
+#define UI_H
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <cstdlib>
+#include <iomanip>
+#include <vector>
+#include <conio.h>
+#include "Signal.h"
+
+#define windowsHeight 40
+#define windowsWidth 120
+#define generalTitleHeight 2
+#define generalBoxHeight 12
+#define calendarTitleHeight 3
+#define calendarBoxHeight (windowsHeight-generalTitleHeight-generalBoxHeight-calendarTitleHeight-commandBoxHeight-bottomBoxHeight -2) 
+							// -2 because don't use the line before command box and the last line of the console
+#define commandBoxHeight 2
+#define bottomBoxHeight 6 //don't use the last line of the console
+#define didUKnowHeight 4
+#define resultBoxHeight (bottomBoxHeight -1 ) //use 1 line to write search result title
+
+#define generalInitY (generalTitleHeight)
+#define generalInitX 0
+#define calendarInitY (generalInitY + generalBoxHeight + calendarTitleHeight)
+#define calendarInitX 0
+#define commandInitY (calendarInitY + calendarBoxHeight +1)
+#define commandInitX 0
+#define inputStartX 8
+#define inputStartY commandInitY
+#define diduknowInitX 0
+#define diduknowInitY (commandInitY + commandBoxHeight)
+#define operationResultY (diduknowInitY + 1)
+#define operationResultX 0
+
+//INDEX_COLOR, DESCRIPTION_COLOR, LOCATION_COLOR, TIME_COLOR, DATE_COLOR, PRIORITY_COLOR
+#define calendarIndexInitX 2
+#define calendarDescriptionInitX 6
+#define calendarLocationInitX (calendarTimeInitX - 20)
+#define calendarTimeInitX 85
+#define calendarDateInitX 99
+#define calendarPriorityInitX 113
+
+#define generalIndexInitX 2
+#define generalDescriptionInitX 6
+#define generalLocationInitX (generalTimeInitX- 10)
+#define generalTimeInitX 113
+#define generalDateInitX 113
+#define generalPriorityInitX 113
+
+#define maxCharLocationCalendar (calendarTimeInitX - calendarLocationInitX -1)
+#define maxCharDetailCalendar (calendarLocationInitX - calendarDescriptionInitX -1)
+#define maxCharLocationGeneral (generalTimeInitX - generalLocationInitX -1)
+#define maxCharDetailGeneral (generalLocationInitX - generalDescriptionInitX -1)
+
+#define INDEX_COLOR FOREGROUND_INTENSITY | FOREGROUND_BLUE
+#define DESCRIPTION_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE
+#define LOCATION_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN
+#define TIME_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN
+#define DATE_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN
+#define PRIORITY_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED
+
+#define FOOTER_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN
+
+#define TAB 9
+#define ENTER 13
+#define BACKSPACE 8
+
+#define maxInputSize (windowsWidth * 2 -9)
+
+using namespace std;
+
+class UI
+{
+private:
+
+	static const string CLEAR_SIGNAL_MESSAGE;
+	static const string ADD_SUCCESSFUL_MESSAGE;
+	static const string UPDATE_SUCCESSFUL_MESSAGE;
+	static const string DELETE_SUCCESSFUL_MESSAGE;
+	static const string ONE_EMPTY_LINE;
+
+	static const string DID_U_KNOW_ADD;
+	static const string DID_U_KNOW_DELETE;
+	static const string DID_U_KNOW_EXIT;
+	static const string DID_U_KNOW_SEARCH;
+	static const string DID_U_KNOW_UPDATE;
+	static const string DID_U_KNOW_UNDO;
+	static const string DID_U_KNOW_HINTS;
+	
+
+	HANDLE hConsole;
+	string input;
+	Signal focusedField;
+	Signal displayMode;
+	Signal diduknowStatus;
+	Signal diduknowPrevStatus;
+	Signal curStatus;
+
+	int currentChar;
+	int generalInitRowIndex;
+	int calendarInitRowIndex;
+	int diduknowInitRowIndex;
+	int generalEndRowIndex; // in case an entry occupies > 1 line, end index is not initial index + box height
+	int calendarEndRowIndex;
+	int diduknowEndRowIndex;
+
+	void setScreenSize();
+	void setBackground();
+	void drawBanner();
+	void gotoCommandBox();
+	void drawCommandBox();
+	void clearBox(int startH, int height);
+	void gotoxy(int x, int y);
+	void writeTitle(string words, int startH, int startW);
+	void writeHighlightedTitle(string words,int startH, int startW);
+	void highlightTitle(int searchBoxSize);
+
+	void changeDisplayMode();
+	void displayNewMode(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+	void changeFocusedField(vector<string>* resultList);
+	void scrollUp(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+	void scrollDown(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+	void traceInput(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+	void setDidUKnowStatus();
+	void initializeDidUKnowStatus();
+
+	void initializeGeneralInitRowIndex(int generalSize);
+	void initializeCalendarInitRowIndex(int calendarSize);
+	void initializeResultInitRowIndex(int resultSize);
+
+	void extractParts(string entry, string* partlist);
+	bool isPartEnoughSpace(int strLength, int maxLength, int spaceLeft);
+	bool isCalendarEntryEnoughSpace(string description, string location, int rowPosition);
+	bool isGeneralEntryEnoughSpace(string description, string location, int rowPosition);
+	void printPart(string part, int maxLength, int initX, int initY, int &endY);
+
+	void printCalendarString(int index, string row, int &rowPosition, bool& isPrinted);
+	void printGeneralString(int index, string row, int &rowPosition, bool& isPrinted);
+	void printResultString(int index, string row, int &rowPosition, int sizeOfGeneral);
+	void printDiduknowHints();
+	bool isGeneral(string row);
+
+	void printContinueFooter();
+	void generalEntryListDisplay(vector<string>* generalEntryList);
+	void calendarEntryListDisplay(vector<string>* calendarEntryList);
+	void resultListDisplay(vector<string>* resultList, int sizeOfGeneral);
+	void diduknowHintDisplay();
+
+	void startingScreenDisplay();
+	void mainScreenDisplay(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+public:
+	UI(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+
+	void userInteract(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+	string retrieveInput();
+	Signal retrieveFocusedField();
+	void displayMessage(string message);
+
+	~UI();
+};
+
+#endif;
