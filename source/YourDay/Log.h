fb1762defdaecc8fd391340144c8ae6847221078
user <user@user-PC.nus.edu.sg>
2012-11-08 20:08:14 +0800

    log bug fixed: now can write exception threw in log.txt

---
+++ source/YourDay/Log.h
@@ -0,0 +1,39 @@
+#ifndef LOG_H
+#define LOG_H
+
+# include <string>
+# include <fstream>
+# include <time.h>
+# include <stdio.h>
+
+# include "Signal.h"
+
+using namespace std;
+
+class Log
+{
+private:
+
+	enum OPEN_TYPE {APP_TYPE, OUT_TYPE};
+	
+	fstream file;
+	void disassociateFile();
+	void associateFile(string fileName, OPEN_TYPE mode);
+	
+public:
+
+	void writeTime();
+	
+	void writeCreated(string objName);
+	void writeRetrieved(string varName);
+	void writeExecuted(string fxName);
+	void writeConditionEntered(string condition, bool boolValue);
+	
+	void writeData(string dataName, string data);
+	void writeData(string dataName, Signal data);
+	void writeData(string dataName, bool data);
+	
+	void writeException(string exception);
+};
+
+#endif
