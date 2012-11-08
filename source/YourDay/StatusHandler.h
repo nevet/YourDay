d27638c9735f1963a7a4455973756cb23a52cb9c
Henry <Henry@Henry-PC>
2012-11-09 02:03:12 +0800

    Update Executor updates. Now the general entry could be updated to calendar lists.

---
+++ source/YourDay/StatusHandler.h
@@ -0,0 +1,13 @@
+#ifndef STATUSHANDLER_H
+#define STATUSHANDLER_H
+
+#include "Signal.h"
+
+class StatusHandler
+{
+public:
+	bool success(Signal signal);
+	bool error(Signal signal);	
+};
+
+#endif
