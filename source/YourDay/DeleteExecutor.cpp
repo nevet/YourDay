ad6eb76b9daaa4fd5983bf11e6c9976d00b1a0c4
user <user@user-PC.nus.edu.sg>
2012-11-09 04:57:50 +0800

    power search function integrated, compilable

---
+++ source/YourDay/DeleteExecutor.cpp
@@ -0,0 +1,60 @@
+#include "DeleteExecutor.h"
+#include <cassert>
+
+DeleteExecutor::DeleteExecutor(vector<string>* generalEntryList, vector<string>* calendarEntryList, string details, Signal focusingField)
+{
+	assert(details!="");
+	assert(generalEntryList!=NULL);
+	assert(calendarEntryList!=NULL);
+	assert(focusingField!=GENERAL || focusingField!=CALENDAR);
+
+	switch (focusingField)
+	{
+		case GENERAL:
+			_focusingEntryList = generalEntryList;
+			_undoFocusingEntryList = *generalEntryList;
+			
+			break;
+		
+		case CALENDAR:
+			_focusingEntryList = calendarEntryList;
+			_undoFocusingEntryList = *calendarEntryList;
+			
+			break;
+			
+		default:
+			break;
+	}
+
+	_details = details;
+}
+
+void DeleteExecutor::execute()
+{
+	int index;
+	
+	vector<string>::iterator position;
+	
+	index=extractIndex(_details);
+
+	if(index < 1)
+	{
+		throw string ("index is not existing\n");
+	}
+	else if(index > (_focusingEntryList->size()))
+	{
+		throw string ("index is larger than list size\n");
+	}
+	else 
+	{
+		position = _focusingEntryList->begin() + index - 1;
+		_focusingEntryList->erase(position);
+		
+		status = DELETE_S;
+	}
+}
+
+void DeleteExecutor::undo()
+{
+	*_focusingEntryList = _undoFocusingEntryList;
+}
