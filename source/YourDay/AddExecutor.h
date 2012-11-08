b0ae91878f6c0fd9972ab7328449004add61a2d2
Ignatius Damai <Ignatius Damai@IgnatiusDamai.nus.edu.sg>
2012-11-09 04:50:43 +0800

    finished the sorting algorithm

--- source/YourDay/AddExecutor.h
+++ source/YourDay/AddExecutor.h
@@ -14,8 +14,13 @@ private:
 	vector<string> _undoGeneralEntryList;
 	vector<string> _undoCalendarEntryList;
 
-	string _details;
+	int extractDay(string date);
+	int extractMonth(string date);
+	int extractYear(string date);
 
+	bool isEarlier(string &entry1, string &entry2);
+	string _details;
+	void quickSort(vector<string> &entryList, int low, int high);
 public:
 	AddExecutor(vector<string>* generalEntryList, vector<string>* calendarEntryList, string details) throw (string);
 
