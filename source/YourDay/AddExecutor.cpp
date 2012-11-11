192434fbd2c3e3c98316e3c8d2c1f9f742b8582d
Ignatius Damai <Ignatius Damai@IgnatiusDamai.nus.edu.sg>
2012-11-11 16:39:55 +0800

    Finished the search, and add to a predefined position.

--- source/YourDay/AddExecutor.cpp
+++ source/YourDay/AddExecutor.cpp
@@ -19,33 +19,66 @@ AddExecutor::AddExecutor(vector<string>* generalEntryList, vector<string>* calen
 	_undoGeneralEntryList = *generalEntryList;
 }
 
-int AddExecutor::partition(vector<string> &entryList, int low, int high)
+int AddExecutor::searchIndex(vector<string>* _entryList, string input)
 {
-	if (low == high) return low;
-	string pivot = entryList[low];
-	
-	int m = low;
-	for ( int k = low+1; k <= high; ++k )
-	{
-		if ( isEarlier(entryList[k], pivot))
-		{		
-			++m;
-			swap(entryList[k], entryList[m]);
-		}
-    }
+	assert(_entryList != NULL);
+	assert(input != "");
+	int listSize = _entryList->size();
 
-  swap(entryList[low], entryList[m]);  	
-  return m;
+	if (listSize == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return binarySearch(_entryList, input, 0, listSize-1);
+	}
 }
 
-void AddExecutor::quickSort(vector<string> &entryList, int low, int high)
+int AddExecutor::binarySearch(vector<string>* _entryList, string key, int imin, int imax)
 {
-	int pivotIdx;
-	if (low < high)
+	assert(_entryList != NULL);
+	assert(key != "");
+	int imid = (imin + imax) / 2;
+	while (imax >= imin)
 	{
-		pivotIdx = partition(entryList, low, high);
-		quickSort(entryList, low, pivotIdx - 1);
-		quickSort(entryList, pivotIdx + 1, high);
+		if (isEarlier((*_entryList)[imid],key))
+		{
+			imin = imid + 1;
+		}
+		else if (isEarlier(key,(*_entryList)[imid]))
+		{
+			imax = imid - 1;
+		}
+		else
+		{
+			return imid;
+		}
+		imid = (imin + imax) / 2;
+	}
+	return imid;
+}
+
+void AddExecutor::addToPosition(vector<string>* _entryList, int index, string input)
+{
+	assert(_entryList != NULL);
+	assert(input != "");
+	int listSize = _entryList->size();
+	int i;
+
+	if (listSize == 0)
+	{
+		_entryList -> push_back(input);
+	}
+	else
+	{
+		string dummyString = "";
+		_entryList -> push_back(dummyString);
+		for (i = (listSize - 1); i > index; i--)
+		{
+			(*_entryList)[i] = (*_entryList)[i-1];
+		}
+		(*_entryList)[index+1] = input;
 	}
 }
 
@@ -151,10 +184,9 @@ void AddExecutor::execute() throw (string)
 	}
 	else
 	{
-		_calendarEntryList -> push_back(_details);
-		_resultList->push_back(_details);
-
-		quickSort(*_calendarEntryList,0,_calendarEntryList->size()-1);
+		int index = searchIndex(_calendarEntryList, _details);
+		addToPosition(_calendarEntryList, index, _details);
+		addToPosition(_resultList,index,_details);
 	}
 }
 
--- source/YourDay/AddExecutor.h
+++ source/YourDay/AddExecutor.h
@@ -16,14 +16,19 @@ private:
 	vector<string> _undoGeneralEntryList;
 	vector<string> _undoCalendarEntryList;
 
-	static int extractDay(string date);
-	static int extractMonth(string date);
-	static int extractYear(string date);
+	int extractDay(string date);
+	int extractMonth(string date);
+	int extractYear(string date);
 
-	static bool isEarlier(string &entry1, string &entry2);
+	bool isEarlier(string &entry1, string &entry2);
 	string _details;
-	int partition(vector<string> &entryList, int low, int high);
-	void quickSort(vector<string> &entryList, int low, int high);
+
+	
+	int searchIndex(vector<string>* _entryList, string input);
+	int binarySearch(vector<string>* _entryList, string key, int imin, int imax);
+	void addToPosition(vector<string>* _entryList, int position, string input);
+
+
 public:
 	AddExecutor(vector<string>* generalEntryList, vector<string>* calendarEntryList, vector<string>* resultList, string details);
 
