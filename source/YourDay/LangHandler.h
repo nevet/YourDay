94de09f64416231eab342da30c692815c99384b6
Ignatius Damai <ignatiusdamai@gmail.com>
2012-11-11 19:33:36 +0800

    updated Search Executor

---
+++ source/YourDay/LangHandler.h
@@ -0,0 +1,167 @@
+/**
+    YourDay
+    LangHandler.h
+    Purpose: Calculates the total of 6 checks
+
+    @author a0088455
+    @version 0.1 10/13/2012
+*/
+#ifndef LANGHANDLER_H
+#define LANGHANDLER_H
+
+#include <iostream>
+#include <vector>
+
+#include "Signal.h"
+#include "StatusHandler.h"
+#include "StorageHandler.h"
+#include "Executor.h"
+#include "Log.h"
+#include "AddExecutor.h"
+#include "DeleteExecutor.h"
+#include "SearchExecutor.h"
+#include "UpdateExecutor.h"
+#include "ExitExecutor.h"
+
+using namespace std;
+
+class LangHandler
+{
+private:
+
+	StatusHandler sh;
+	
+	Log log;
+
+	Signal command;
+	Signal langStatus;
+
+	string details;
+
+	const static int MONTH[12];
+	
+	const static string NULL_STRING;
+	const static string PRIORITY_INDICATOR;
+	const static string LOCATION_INDICATOR;
+	const static string SPACE_BAR;
+	const static string DELIMINATOR;
+
+	bool isLeap(int year);
+	bool isDate(string date);
+	bool isTime(string time);
+	bool isInt(string inx);
+	bool isLogicDate(string date);
+	bool isLogicTime(string time);
+	bool isLogicPriority(string priority);
+
+	/**
+	* This operation is used to eliminate leading and trailing spaces of a
+	* string.
+	* 
+	* @param str
+	*			is the string needs to be spaces-freed
+	*/
+	void eliminateSpaces(string& str);
+
+	/**
+	* Encodes the raw string into the correct saving format
+	*
+	* @param input
+	*			is a raw string that needs to be encoded
+	* @param command
+	*			is the command type
+	* @return encoded string, format of the string is:
+	* 
+	* #index#description#location#time#date#priority#
+	*
+	* index:
+	*		this field contains index information required by delete and update
+	*		functions.
+	* description:
+	*		this field contains description of the task required by add, search
+	*		and update functions.
+	* location:
+	*		this field contains location information required by add and search
+	*		functions.
+	* time:
+	*		this field contains time information required by add and search
+	*		functions.
+	* date:
+	*		this field contains date information required by add and search
+	*		functions.
+	* priority:
+	*		this field contains priority information required by add and search
+	*		functions.
+	*		
+	* Note that some fields are not necessarily required. However, if one field
+	* if not required for a certain function, that field should not be OMITTED.
+	*
+	* e.g
+	*
+	* userInput = "add CS2103 TUT at COM on Friday"
+	* encoded	= "##CS2103 TUT#COM##Friday##"
+	*
+	* explaination:
+	*
+	* since index field, time field and priority field are missing,
+	* corresponding fields should be left as "blank".
+	*
+	* userInput = "delete 1"
+	* encoded	= "#1######"
+	*
+	* explaination:
+	*
+	* only index field is specified, so other fields should be "blank".
+	*/
+	void encoder(string input, Signal command);
+
+	/**
+	* This operation will set command type using userCommand. If userCommand
+	* took from the user is invalid, COMMAND_E Signal will be set.
+	*/
+	void setCommand(string userCommand);
+
+public :
+	
+	LangHandler();
+
+	/**
+	* Signals will be:
+	* SUCCESS		User input has been successfully proceeded;
+	* Exception signals threw by separate
+	*/
+	Signal getStatus();
+
+	/**
+	* Seperates user input's string into 2 parts, the command and the
+	* encoded string.
+	*
+	* User command will be stored in private string userCommand;
+	* Details info will be stored in private string details
+	* 
+	* e.g
+	*
+	* userInput = "add CS2103 tutorial at COM1";
+	* return = {userCommand = "add", detials = "#CS2103 tutorial#COM1##"}
+	* 
+	* userInput = "delete 1";
+	* return = {userCommand = "delete", details = "#1###"}
+	*
+	* details about format of encoded string can be found in encoded().
+	*/
+	void separate(string userInput) throw (string);
+
+	/**
+	* This operation will pack up an specific executor w.r.t user's command
+	*/
+	Executor* pack(bool* quit, Signal focusingField,
+							   vector<string>* generalEntryList, 
+							   vector<string>* calendarEntryList,
+							   vector<string>* resultList,
+							   StorageHandler* store);
+
+	~LangHandler();
+};
+
+
+#endif
---
+++ source/YourDay/UI.h
@@ -0,0 +1,203 @@
+
+#ifndef UI_H
+#define UI_H
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <cstdlib>
+#include <iomanip>
+#include <vector>
+#include <conio.h>
+#include "Signal.h"
+
+#define WINDOWS_HEIGHT 40
+#define WINDOWS_WIDTH 120
+#define GENERAL_TITLE_HEIGHT 2
+#define GENERAL_BOX_HEIGHT 12
+#define CALENDAR_TITLE_HEIGHT 3
+#define CALENDAR_BOX_HEIGHT (WINDOWS_HEIGHT-GENERAL_TITLE_HEIGHT-GENERAL_BOX_HEIGHT-CALENDAR_TITLE_HEIGHT-COMMAND_BOX_HEIGHT-BOTTOM_BOX_HEIGHT -1) 
+// -1 because don't use the line before command box
+#define COMMAND_BOX_HEIGHT 2
+#define BOTTOM_BOX_HEIGHT 7 
+#define DIDUKNOW_HEIGHT 4
+#define RESULT_BOX_HEIGHT (WINDOWS_HEIGHT - OPERATION_RESULT_Y -1)
+
+#define GENERAL_INIT_Y (GENERAL_TITLE_HEIGHT)
+#define GENERAL_INIT_X 0
+#define CALENDAR_INIT_Y (GENERAL_INIT_Y + GENERAL_BOX_HEIGHT + CALENDAR_TITLE_HEIGHT)
+#define CALENDAR_INIT_X 0
+#define COMMAND_INIT_Y (CALENDAR_INIT_Y + CALENDAR_BOX_HEIGHT +1)
+#define COMMAND_INIT_X 0
+#define INPUT_START_X 8
+#define INPUT_START_Y COMMAND_INIT_Y
+#define DIDUKNOW_INIT_X 0
+#define DIDUKNOW_INIT_Y (COMMAND_INIT_Y + COMMAND_BOX_HEIGHT)
+#define OPERATION_RESULT_Y (DIDUKNOW_INIT_Y +1)
+#define OPERATION_RESULT_X 0
+
+#define NUMBER_OF_ENTRY_PARTS 6
+
+//INDEX_COLOR, DESCRIPTION_COLOR, LOCATION_COLOR, TIME_COLOR, DATE_COLOR, PRIORITY_COLOR
+#define CALENDAR_INDEX_INIT_X 2
+#define CALENDAR_DESCRIPTION_INIT_X 6
+#define CALENDAR_LOCATION_INIT_X 65
+#define CALENDAR_TIME_INIT_X 85
+#define CALENDAR_DATE_INIT_X 99
+#define CALENDAR_PRIORITY_INIT_X 113
+
+#define GENERAL_INDEX_INIT_X 2
+#define GENERAL_DESCRIPTION_INIT_X 6
+#define GENERAL_LOCATION_INIT_X 85
+#define GENERAL_TIME_INIT_X 113
+#define GENERAL_DATE_INIT_X 113
+#define GENERAL_PRIORITY_INIT_X 113
+
+#define INDEX_COLOR FOREGROUND_INTENSITY | FOREGROUND_BLUE
+#define DESCRIPTION_COLOR FOREGROUND_INTENSITY |FOREGROUND_BLUE | FOREGROUND_GREEN
+#define LOCATION_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_RED
+#define TIME_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN
+#define DATE_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN 
+#define PRIORITY_COLOR FOREGROUND_INTENSITY | FOREGROUND_RED
+
+#define FOOTER_COLOR FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED
+
+#define TAB 9
+#define ENTER 13
+#define BACKSPACE 8
+
+#define MAX_INPUT_SIZE (WINDOWS_WIDTH * 2 -9)
+
+using namespace std;
+
+class UI
+{
+private:
+
+	static const string CLEAR_SIGNAL_MESSAGE;
+	static const string ADD_SUCCESSFUL_MESSAGE;
+	static const string UPDATE_SUCCESSFUL_MESSAGE;
+	static const string DELETE_SUCCESSFUL_MESSAGE;
+	static const string ONE_EMPTY_LINE;
+
+	static const string DID_U_KNOW_ADD;
+	static const string DID_U_KNOW_DELETE;
+	static const string DID_U_KNOW_EXIT;
+	static const string DID_U_KNOW_SEARCH;
+	static const string DID_U_KNOW_UPDATE;
+	static const string DID_U_KNOW_UNDO;
+	static const string DID_U_KNOW_HINTS;
+
+	HANDLE hConsole;
+	string input;
+	Signal focusedField;
+	Signal generalDisplayMode;
+	Signal calendarDisplayMode;
+	Signal resultDisplayMode;
+	Signal diduknowStatus;
+	Signal diduknowPrevStatus;
+	Signal prevCommand;
+
+	int currentChar;
+	bool isResultDisplay;
+
+	vector<int> generalInitArrayPart;
+	vector<int> generalInitArrayFull;
+	vector<int> calendarInitArrayPart;
+	vector<int> calendarInitArrayFull;
+	vector<int> resultInitArrayPart;
+	vector<int> resultInitArrayFull;
+
+	int indexCurGeneralInitArray;
+	int indexCurCalendarInitArray;
+	int indexCurResultInitArray;
+
+	int highlightGeneralRowIndex;
+	int highlightCalendarRowIndex;
+
+	void setScreenSize();
+	void setBackground();
+	void drawBanner();
+	void gotoCommandBox();
+	void drawCommandBox();
+	void clearBox(int startH, int height);
+	void gotoxy(int x, int y);
+	void writeTitle(string words, int startH, int startW);
+	void writeHighlightedTitle(string words,int startH, int startW);
+	void highlightTitle();
+
+	void initializeDidUKnowStatus();
+	void initializeInitArrayIndices();
+	void initializeDisplayModes();
+	void initializeFocusedField();
+
+	void extractParts(string entry, string* partArray);
+	int countPartLine(string part, int maxLength);
+	bool isGeneral(string row);
+	void setGeneralInitArrayPart(vector<string>* generalEntryList);
+	void setGeneralInitArrayFull(vector<string>* generalEntryList);
+	void setCalendarInitArrayPart(vector<string>* calendarEntryList);
+	void setCalendarInitArrayFull(vector<string>* calendarEntryList);
+	void setResultInitArrayPart(vector<string>* resultList);
+	void setResultInitArrayFull(vector<string>* resultList);
+	void setInitialIndexArrays(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+
+	int getGeneralInitIndex();
+	int getNextGeneralInitIndex(bool& isValid);
+	int getCalendarInitIndex();
+	int getNextCalendarInitIndex(bool& isValid);
+	int getResultInitIndex();
+	int getNextResultInitIndex(bool& isValid);
+	int findNearestInitArrayIndex(vector<int>* initialIndexArray, int entryIndex);
+
+	void changeDisplayMode();
+	void displayNewMode(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+	void changeFocusedField();
+	void scrollUp(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+	void scrollDown(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+	void setDidUKnowStatus();
+
+	void printLimitedLengthPart(string part, int maxLength, int initX, int initY, int& endPosition);
+	void printEntryPartMode(int* positionArray, int* colorArray, string* partArray, int index, int rowPosition);
+	void printEntryFullMode(int* positionArray, int* colorArray, string* partArray, int index, int& rowPosition);
+
+	void printCalendarEntry(int index, string row, int &rowPosition);
+	void printGeneralEntry(int index, string row, int &rowPosition);
+	void printResultEntry(int index, string row, int &rowPosition);
+	void printDiduknowHints();
+
+	void printGeneralFooter();
+	void printCalendarFooter();
+	void printResultFooter();
+
+	void generalEntryListDisplay(vector<string>* generalEntryList);
+	void calendarEntryListDisplay(vector<string>* calendarEntryList);
+	void resultListDisplay(vector<string>* resultList);
+	void diduknowHintDisplay();
+
+	void processResultList(vector<string>* resultList, string& info);
+	void handleResultInfo(string info, vector<string>* generalList, vector<string>* calendarList);
+	void processAddUpdateInfo(string info, vector<string>* generalList, vector<string>* calendarList);
+	void processSearchInfo(string info, string& keyWord, vector<string>* suggestion);
+	void printSearchInfo(string key, vector<string>* suggestion);
+
+	void handleInitialGeneralIndexOverflow();
+	void handleInitialCalendarIndexOverflow();
+	void handleInitialResultIndexOverflow();
+	void handleInitialIndicesOverflow();
+
+	void startingScreenDisplay();
+public:
+	UI(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+
+	void mainScreenDisplay(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+	void traceInput(vector<string>* calendarEntryList, vector<string>* generalEntryList, vector<string>* resultList);
+
+	string retrieveInput();
+	Signal retrieveFocusedField();
+	void displayMessage(string message);
+
+	~UI();
+};
+
+#endif;
