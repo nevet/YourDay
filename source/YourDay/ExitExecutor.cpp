47259a44f7e4de6ae38cc991b7112f5f77f80358
Ignatius Damai <Ignatius Damai@IgnatiusDamai>
2012-11-12 21:22:54 +0800

    Refactored my part. Warning: not compilable Waiting for Huang Da's Part

---
+++ source/YourDay/ExitExecutor.cpp
@@ -0,0 +1,25 @@
+#include "ExitExecutor.h"
+#include <cassert>
+ExitExecutor::ExitExecutor(vector<string>* generalEntryList,
+						   vector<string>* calendarEntryList,
+						   StorageHandler* store, bool* quit)
+{	
+	assert(generalEntryList!=NULL);
+	assert(calendarEntryList!=NULL);
+	assert(store!=NULL);
+	assert(quit!=NULL);
+
+	_undoEnable = false;
+
+	_generalEntryList = generalEntryList;
+	_calendarEntryList = calendarEntryList;
+	_store = store;
+	_quit = quit;
+}
+
+void ExitExecutor::execute()
+{
+	_store->writeData(_generalEntryList, _calendarEntryList);
+	
+	*_quit = true;
+}
