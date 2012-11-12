e15dec9997a0d12b4589d6cf8bfb5be069b5cf37
Ignatius Damai <Ignatius Damai@IgnatiusDamai>
2012-11-12 20:14:53 +0800

    Cleaning up the repo

---
+++ source/YourDay/AddExecutor.cpp
@@ -0,0 +1,213 @@
+#include "AddExecutor.h"
+#include <cassert>
+#include <sstream>
+#include <algorithm>
+
+AddExecutor::AddExecutor(vector<string>* generalEntryList, vector<string>* calendarEntryList, vector<string>* resultList, string details)
+{
+	assert(details!="");
+	assert(generalEntryList!=NULL);
+	assert(calendarEntryList!=NULL);
+
+	_generalEntryList = generalEntryList;
+	_calendarEntryList = calendarEntryList;
+	_resultList = resultList;
+	_details = details;
+
+	//a local copy of entry list for undo using
+	_undoCalendarEntryList = *calendarEntryList;
+	_undoGeneralEntryList = *generalEntryList;
+}
+
+//@author A0088455R
+/**
+* Manipulates the entry string date and returns it in YYYY/MM/DDHH:MM format
+* This is done through extraction of each field then reverse the fields using
+* os stream
+**/
+string AddExecutor::manipulateEntryDate(string entry)
+{
+	//assert(entry != "");
+
+	ostringstream dateContender;
+	ostringstream monthContender;
+	ostringstream output;
+
+	string timeRange = extractTime(entry);
+	string entryDate = extractDate(entry);
+
+	int entryDay = extractDay(entryDate);
+	int entryMonth = extractMonth(entryDate);
+	int entryYear = extractYear(entryDate);	
+
+	if (entryDay < 10)
+	{
+		dateContender<<"0"<<entryDay;
+	}
+	else
+	{
+		dateContender<<entryDay;
+	}
+	
+	if (entryMonth < 10)
+	{
+		monthContender<<"0"<<entryMonth;
+	}
+	else
+	{
+		monthContender<<entryMonth;
+	}
+
+	output<<entryYear<<monthContender.str()<<dateContender.str()<<timeRange;
+	return output.str();
+}
+
+/**
+* Checks whether the entry is earlier than entry 2.
+* first the entries will be manipulated then checked
+**/
+bool AddExecutor::isEarlier(string &entry1, string &entry2)
+{
+	//assert(entry1 != "");
+	//assert(entry2 != "");
+
+	string entry1Date = manipulateEntryDate(entry1);
+	string entry2Date = manipulateEntryDate(entry2);
+
+	
+
+	return entry1Date < entry2Date;
+}
+/**
+* This method calls the binary search functions to find the correct index
+* where does the user's encoded input should be added
+* if the list size is 0, it simply returns 0 (the first index)
+**/
+int AddExecutor::searchIndex(vector<string>* _entryList, string input)
+{
+	assert(_entryList != NULL);
+	assert(input != "");
+	int listSize = _entryList->size();
+
+	if (listSize == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return binarySearch(_entryList, input, 0, listSize-1);
+	}
+}
+
+/**
+* This is the implementation of the binary search
+* The algorithm is an optimized version of binary search
+* The implementation is using the nonrecursive version to support larger
+* batch processing
+**/
+int AddExecutor::binarySearch(vector<string>* _entryList, string key, int imin, int imax)
+{
+	assert(_entryList != NULL);
+	assert(key != "");
+
+	int imid;
+
+	//First we find the mid point
+	imid = (imin + imax) / 2;
+
+	//To have a correct index, we ned to add 1 if imin+imax is not
+	// dividable by 2
+	if ((( imin + imax) % 2 ) == 1)
+	{
+		imid += 1;
+	}
+	while (imax >= imin)
+	{
+
+		if (isEarlier((*_entryList)[imid],key))
+		{
+			imin = imid + 1;
+		}
+		else if (isEarlier(key,(*_entryList)[imid]))
+		{
+			imax = imid - 1;
+		}
+		else
+		{
+			return imid;
+		}		
+
+		//Repeat the imid division process
+		imid = (imin + imax) / 2;
+		if ((( imin + imax) % 2 ) == 1)
+		{
+			imid += 1;
+		}
+	}
+	return imid;
+}
+
+void AddExecutor::shiftList(vector<string>* _entryList, int index)
+{
+		int i;
+		string dummyString = "";
+		_entryList -> push_back(dummyString);
+
+		int listSize = _entryList->size();
+		for (i = (listSize - 1); i > index; i--)
+		{
+			(*_entryList)[i] = (*_entryList)[i-1];
+		}
+}
+/**
+* Adds the encoded input to the correct position
+* if the vector size is 0, we simly push the input
+* other wise we shift the entry from index position by 1
+* then adds the string at the position
+**/
+void AddExecutor::addToPosition(vector<string>* _entryList, int index, string input)
+{
+	assert(_entryList != NULL);
+	assert(input != "");
+	int listSize = _entryList->size();
+
+	if (listSize == 0)
+	{
+		_entryList -> push_back(input);
+	}
+	else
+	{
+		shiftList(_entryList, index);		
+		(*_entryList)[index] = input;
+	}
+}
+
+
+void AddExecutor::execute() throw (string)
+{
+	bool isCalendarEntry = false;
+
+	if (_details == "")
+	{
+		throw string ("Cannot add an empty entry\n");
+	}
+
+	if (extractDate(_details) == "")
+	{
+		_generalEntryList->push_back(_details);
+		_resultList->push_back(_details);
+	}
+	else
+	{
+		int index = searchIndex(_calendarEntryList, _details);
+		addToPosition(_calendarEntryList, index, _details);
+		addToPosition(_resultList, index, _details);
+	}
+}
+
+void AddExecutor::undo()
+{
+	*_generalEntryList = _undoGeneralEntryList;
+	*_calendarEntryList = _undoCalendarEntryList;
+	
+}
---
+++ source/YourDay/StorageHandler.cpp
@@ -0,0 +1,135 @@
+#include <cassert>
+#include <direct.h>
+
+#include "StorageHandler.h"
+//@author A0091734A
+
+using namespace std; 
+
+
+const string StorageHandler::ENTRY_STORE_FORMAT = "%s %s %s\n";
+const string StorageHandler::FILEPATH = "YourdayBin/";
+
+const string StorageHandler::DataBaseCalendarFile = "YourDayCEntry.yd";
+const string StorageHandler::DataBaseGeneralFile = "YourDayGEntry.yd";
+//@author A0091734A 
+StorageHandler::StorageHandler()
+{
+	_mkdir("YourdayBin");
+
+	if(!checkFileExistence(FILEPATH, DataBaseGeneralFile))
+		ofstream writeFile(FILEPATH + DataBaseGeneralFile);
+
+	if(!checkFileExistence(FILEPATH, DataBaseCalendarFile))
+		ofstream writeFile(FILEPATH + DataBaseCalendarFile);
+}
+
+void StorageHandler::setStatus()
+{
+	status = CLEAR;
+}
+//read data from database
+void StorageHandler::readData(vector<string> *ramForGeneralList, vector<string>  *ramForCalendarList)
+{	
+	assert(ramForGeneralList!=NULL);
+	assert(ramForCalendarList!=NULL);
+
+	ifstream infileG(FILEPATH + DataBaseGeneralFile);
+	string textLine;
+	while(getline(infileG,textLine))
+	{
+		ramForGeneralList->push_back(textLine);
+	}
+	infileG.close();
+
+	ifstream infileC(FILEPATH + DataBaseCalendarFile);
+	while(getline(infileC,textLine))
+	{
+		ramForCalendarList->push_back(textLine);
+	}
+	infileC.close();
+
+	return ;
+}
+//Write Data into the database
+void StorageHandler::writeData(vector<string> *ramForGeneralList, vector<string>  *ramForCalendarList)
+{
+	assert(ramForGeneralList!=NULL);
+	assert(ramForCalendarList!=NULL);
+
+	ofstream clearFileG(FILEPATH + DataBaseGeneralFile);
+	ofstream outfileG(FILEPATH + DataBaseGeneralFile,ofstream::app);
+	
+	for(int i=0;i<ramForGeneralList->size();i++)
+	{
+		outfileG<<(ramForGeneralList->at(i))<<endl;
+	}
+	
+	
+
+	ofstream clearFileC(FILEPATH + DataBaseCalendarFile);
+	ofstream outfileC(FILEPATH + DataBaseCalendarFile,ofstream::app);
+
+	for(int i=0;i<ramForCalendarList->size();i++)
+	{
+		outfileC<<(ramForCalendarList->at(i))<<endl;
+	}
+	outfileG.close();
+	outfileC.close();
+
+	return ;
+}
+
+bool StorageHandler::checkFileExistence(string filePath, string fileName)
+{
+	fstream testFile;
+	bool isExist;
+	associateFile(filePath, fileName, testFile, OPEN_TYPE::IN_TYPE);
+	isExist=testFile.is_open();
+	disassociateFile(testFile);
+	return isExist;
+}
+
+void StorageHandler::disassociateFile(fstream & file)
+{
+	if (file.is_open()) file.close();
+}
+
+void StorageHandler::associateFile(string filePath, string fileName,
+	fstream & file, OPEN_TYPE mode)
+{
+	string fileFullPath(filePath+fileName);
+	switch (mode)
+	{
+	case APP:
+		file.open(fileFullPath.c_str(), fstream::app);
+		break;
+	case IN_TYPE:
+		file.open(fileFullPath.c_str(), fstream::in);
+		break;
+	case OUT_TYPE:
+		file.open(fileFullPath.c_str(), fstream::out);
+		break;
+	default:
+		break;
+	}
+}
+
+void StorageHandler::deleteFile(string filePath, string fileName)
+{
+	string fullPath(filePath+fileName);
+	remove(fullPath.c_str());
+}
+
+void StorageHandler::renameFile(string filePath, string oriName, string newName)
+{
+	string oriFullPath(filePath+oriName);
+	string newFullPath(filePath+newName);
+	rename(oriFullPath.c_str(), newFullPath.c_str());
+}
+
+void StorageHandler::replaceFile(string oriPath, string oriName, string repName)
+{
+	deleteFile(oriPath, oriName);
+	renameFile(oriPath, oriName, repName);
+}
